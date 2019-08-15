2451037bfb3828c115ef80e783aa1f291054a1e9
Alexander <https://github.com/AlexDev13>
2019-08-15 11:55:09 +0600

    Factorial 1/2 complete

--- codewars/Factorial.go
+++ codewars/Factorial.go
@@ -5,57 +5,47 @@ package codewars
 import (
 	"fmt"
 	"math"
+	"strconv"
 )
 
-func FactorialExample(f int) {
+func FactorialExample(f int) string {
+	var result int
+	var sum int
+	var str string
+
 	for i := 2; i <= f; i++ {
 		isPrime := true
 
 		for j := 2; j < i; j++ {
 
-			if i%j == 0 ||  math.Sqrt(float64(i))!= 0{
+			if i%j == 0 {
 
 				isPrime = false
 			}
 		}
 
 		if isPrime == true {
-			
-			
-				fmt.Println(i)
-			
-			//res:= f/i
-			
-			//fmt.Println(i, res)
-		}
-	}
 
+			for k := 1; k <= f; k++ {
+
+				result = f / Pow(i, k)
+
+				sum += result
+
+			}
+			str1 := strconv.Itoa(i)
+			str2 := strconv.Itoa(sum)
+		
+			str = str1 + "^" + str2 + " * "
+			fmt.Print(str)
+
+		}
+		sum = 0
+	}
+	return str
 }
 
-// func Example(a int) {
-// 	//count :=1
-// 	for i := 2; i < a; i++ {
-// 		for n := 1; n < i; n++ {
-// 			res := a/Pow(i, n)
-// 			if res == 0 {
-// 				continue
-// 				}else{
-// 					fmt.Println(res)
-// 				}	
-// 			fmt.Println(i," - ",res)
-// 		}
-
-// 	//fmt.Println(count)
-// 	}
-
-// }
-
-// func Pow(a, b int) int {
-// 	x :=math.Pow(float64(a), float64(b))
-// 	return int(x)
-// }
-func Ex(f int){
-	for i:=2;i<=f;i++{
-		fmt.Println(i," - ",f/i)
-	}
+func Pow(a, b int) int {
+	res := math.Pow(float64(a), float64(b))
+	return int(res)
 }
